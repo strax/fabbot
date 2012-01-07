6898439ad28397a1ef92f13eab6a738799edffb3
liquidproof <sami.matias.kukkonen@gmail.com>
2012-01-07 20:20:38 +0200

    Persist with redis

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-var irc = require('irc')
+var irc = require('irc'),
+    redis = require('redis'),
+    util = require('util'),
+    db = redis.createClient()
 
 var config = JSON.parse(require('fs').readFileSync('./config.json'))
-
-var users = { }
 
 var bot = new irc.Client(config.host, config.nick, {
   port: config.port,
   debug: true,
-  channels: config.channels
+  channels: config.channels,
+  realName: 'Fabbot',
+  userName: 'fabbot'
 })
 
 bot.on('error', function(message) {
   console.error('ERROR: %s: %s', message.command, message.args.join(', '));
 })
 
-bot.on('message#vana', function(from, message) {
-  if(/^\!counts/.test(message)) {
-    var counts = []
-    for(i in users)
-      counts.push(i + ": " + users[i])
-    
-    bot.say("#vana", "Counts: " + counts.join(", "))
+bot.on('message', function(from, to, message) {
+  if(/^\!counts/i.test(message)) {
+    db.hgetall("stats:" + to, function(err, stats) { 
+      var counts = []
+      for(user in stats) counts.push(user + ": " + stats[user])
+           
+      bot.notice(from, to + " word counts: " + counts.join(", "))
+    })
   } else {
     var count = message.split(' ').length || 1
-    if(!users[from]) users[from] = 0
-    users[from] += count
-    console.log("%s: %d", from, users[from])
+    
+    db.hincrby("stats:" + to, from, count, redis.print);
   }
 })
